c2dab1e6bee6073470191183907182ac6876480f
Shakhriyor <shakh2616@gmail.com>
2026-03-05 11:53:20 +0500

    Keyboard bilan ishlash funksiyasi qo'shildi

--- script.js
+++ script.js
@@ -19,3 +19,15 @@ function calculate(){
         display.value = "Error";
     }
 }
+
+document.addEventListener("keydown", (e) => {
+    if(!isNaN(e.key) || ['+', '-', '*', '/', '.'].includes(e.key)){
+        appendValue(e.key);
+    } else if(e.key === "Enter"){
+        calculate();
+    } else if(e.key === "Backspace"){
+        deleteLast();
+    } else if(e.key === "Escape"){
+        clearDisplay();
+    }
+});
